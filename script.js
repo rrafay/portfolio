1f7a0db4202ac57e1434cfb6011a993098b0f6f6
Rafay <arafaymbir@gmail.com>
2020-05-28 16:29:46 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.querySelector(".fifth");
+
+button.addEventListener("click", function(){
+    //console.log("clicked")
+
+    document.getElementById("scroll").scrollIntoView({behavior: "smooth"});
+
+
+
+});
